c6767ed2740d4a807f5eb9129dc9bcfec395a44c
maurobiazutti <mauro_biazutti@hotmail.com>
2022-07-26 20:57:20 -0300

    add rg

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_26_010834) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_26_235705) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_26_010834) do
     t.integer "idade"
     t.string "cpf"
     t.string "telefone"
+    t.string "genero"
   end
 
 end
