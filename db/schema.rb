0272b2ff9ae4540e66e490408fd51446a7aa6770
maurobiazutti <mauro_biazutti@hotmail.com>
2022-07-27 19:40:22 -0300

    add peso schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_26_235705) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_27_223405) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
