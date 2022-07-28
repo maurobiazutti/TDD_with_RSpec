4eb83334cb443520aaaecb7dbd49d8e778eb17a5
maurobiazutti <mauro_biazutti@hotmail.com>
2022-07-28 18:32:07 -0300

    add endereço

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :idade, :cpf, :telefone, :genero, :peso)
+      params.require(:user).permit(:name, :email, :idade, :cpf, :telefone, :genero, :peso, :endereco)
     end
 end
