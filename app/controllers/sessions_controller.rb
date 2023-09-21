9f1be05fc5daef6078005bdd22fc69f98ce388a9
mauro <mauro_biazutti@hotmail.com>
2023-09-21 17:16:40 -0300

    create controller session + test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  
   def signin
   end
 
@@ -8,9 +9,28 @@ class SessionsController < ApplicationController
   def signout
   end
 
+  def create
+    user = User.new(session_params)
+    if user.save
+      redirect_to home_index_path
+      return
+    end
+    render json: {}, status: :unauthorized
+  end 
+
   def login
+    user = User.find_by(email: session_params[:email])
+    
+    if user && user.authenticate(session_params[:password])
+      redirect_to home_index_path
+      return
+    end
+    render json: {}, status: :unauthorized 
   end
 
-  def create
+  private
+
+  def session_params
+    params.require(:session).permit( :name, :email, :password, :password_confirmation )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
   get 'sessions/signin'
   get 'sessions/signup'
-  get 'sessions/signout'
-  post 'sessions/login'
   post 'sessions/create'
-
+  post 'sessions/login'
+  
+  post 'sessions/signout'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
